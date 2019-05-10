f0a55e536ab3f99adbef1e2dff1023d2ba436fd5
Nipuni1995 <137nipuniweerasinghe@gmail.com>
2019-05-11 00:16:04 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,69 @@
+  
+        
+		<div class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<ul class="footer_box">
+							<h4>Products</h4>
+							<li><a href="#">APPLE</a></li>
+							<li><a href="#">SAMSUNG</a></li>
+							<li><a href="#">HUAWEI</a></li>
+                            <li><a href="#">SONY</a></li>
+                            <li><a href="#">NOKIA</a></li>
+							<li><a href="#">OTHER</a></li>
+
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<ul class="footer_box">
+							<h4>About</h4>
+							<li><a href="#">Jobs</a></li>
+							<li><a href="#">Discounts</a></li>
+							<li><a href="#">team</a></li>
+							<li><a href="#">Catalog Request/Download</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<ul class="footer_box">
+							<h4>Customer Support</h4>
+							<li><a href="#">Contact Us</a></li>
+							<li><a href="#">Shipping and Order Tracking</a></li>
+							<li><a href="#">Easy Returns</a></li>
+							<li><a href="#">Warranty</a></li>
+							<li><a href="#">Replacement Binding Parts</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<ul class="footer_box">
+							<h4>Follow Us</h4>
+							<div class="footer_search">
+				    		  <!-- <form>
+				    			<input type="text" value="Enter your email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your email';}">
+				    			<input type="submit" value="Go">
+				    		   </form>-->
+					        </div>
+							<ul class="social">	
+							  <div class="fb-like" data-href="https://www.facebook.com/Hackmeplz-1413106622056661/?ref=br_rs" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" width="auto" data-share="true"></div>										  				
+						    </ul>
+						</ul>
+					</div>
+				</div>
+				<div class="row footer_bottom">
+				    <div class="copy">
+                        <p>Developed by <a href="http://tresetsolutions.com/" target="_blank">Treset Solutions</a>© 2017 www.BackCovers.lk. All rights reserved | Delivery : 0777 262 721</p>
+		            </div>
+   				</div>
+			</div>
+		</div>
+		<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.10';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<div class="header">
+		<div class="container">
+			<div class="row">
+			  <div class="col-md-12 col-sm-24">
+					 <div class="logo">
+						<a href="index.php"><img class="mylogo" src="images/logonew1.jpg" alt=""/></a>
+					 </div>
+					 
+					 <div class="menu">
+						  <a class="toggleMenu" href="#"><img src="images/nav.png" alt="" /></a>
+						    <ul class="nav" id="nav">
+						    	<li><a href="index.php">home</a></li>
+						    	<li><a href="about.php">about</a></li>
+<!--
+                                <li><a href="about.php">Gyde</a></li>
+-->
+						    	<li><a href="#apple">apple</a></li>
+						    	<li><a href="#samsung">samsung</a></li>
+						    	<li><a href="#huawei">huawei</a></li>
+								<li><a href="#sony">sony</a></li>	
+                                <li><a href="#nokia">nokia</a></li>	
+								<li><a href="#others">other</a></li>
+                                <li><a href="contact.php">Contact us</a></li>
+                                <li ><a href="#"><img width="40px" height="40px" src="images/cart.png"/></a></li>
+
+								<div class="clear"></div>
+							</ul>
+							<script type="text/javascript" src="js/responsive-nav.js"></script>
+				    </div>							
+	    		    <div class="clear"></div>
+	      </div>
+		 </div>
+	    </div>
+	</div>
